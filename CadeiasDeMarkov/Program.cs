8f9abd950d4a1ac3c6ce5b88541e3ad7be7baa86
rlopes <rlopes@DEVELOPERVM>
2016-10-23 23:00:35 -0200

    Calculando P de N

--- CadeiasDeMarkov/Program.cs
+++ CadeiasDeMarkov/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting;
 
 namespace CadeiasDeMarkov
 {
@@ -16,35 +17,48 @@ namespace CadeiasDeMarkov
             Console.WriteLine("Digite n: ");
             int n = int.Parse(Console.ReadLine());
 
-            var pDeN = CalculaPDeN(matrizP, n);
+            var pDeN = CalculaPdeN(matrizP, matrizP, n);
 
-            for(int i = 0; i < 3; i++)
-                for(int j = 0; j < 3; j++)
-            {
-                Console.WriteLine(string.Format("{0}{1} : {2}",i, j, Math.Round(pDeN[i, j], 3)));
-            }
+            ImprimeMatriz(pDeN, n);
 
             Console.ReadLine();
         }
 
-        private static double[,] CalculaPDeN(double[,] matrizP, int n)
+        private static void ImprimeMatriz(double[,] pDeN, int n)
         {
+            Console.WriteLine($"****P({n}) : ****");
+            Console.Write($"[{Math.Round(pDeN[0, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[0, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[0, 2], 3)}]");
+            Console.Write($"[{Math.Round(pDeN[1, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[1, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[1, 2], 3)}]");
+            Console.Write($"[{Math.Round(pDeN[2, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[2, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[2, 2], 3)}]");
+        }
+
+        private static double[,] CalculaPdeN(double[,] matrizP, double[,] matrizB, int n)
+        {
+            if (n == 1)
+                return matrizP;
+
             var resultado = new double[3, 3];
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
                 {
-                    resultado[i, j] = ProdutoEscalar(i, j, matrizP);
+                    resultado[i, j] = ProdutoEscalar(i, j, matrizP, matrizB);
                 }
 
-            return resultado;
+            return CalculaPdeN(resultado, matrizP, n-1);
         }
 
-        private static double ProdutoEscalar(int i, int j, double[,] matrizP)
+        private static double ProdutoEscalar(int i, int j, double[,] matrizA, double[,] matrizB)
         {
             double total = 0;
             for(int x = 0; x < 3; x++)
             {
-                total += matrizP[i, x] * matrizP[x, j];
+                total += matrizA[i, x] * matrizB[x, j];
             }
 
             return total;
@@ -55,7 +69,7 @@ namespace CadeiasDeMarkov
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.WriteLine(string.Format("Digite o valor [{0}][{1}] da matriz :", i, j));
+                    Console.WriteLine($"Digite o valor [{i}][{j}] da matriz :");
                     var v = Console.ReadLine();
                     matrizP[i, j] = double.Parse(v);
                 }
