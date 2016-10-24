ccd02f5707fec949404f443d0c51a848abdbddc4
rlopes <rlopes@DEVELOPERVM>
2016-10-24 01:16:53 -0200

    Finalizando

--- CadeiasDeMarkov/Program.cs
+++ CadeiasDeMarkov/Program.cs
@@ -8,49 +8,52 @@ namespace CadeiasDeMarkov
         static void Main(string[] args)
         {
             var matrizP = new double[3, 3];
+            var matrizM = new double[1,3];
 
             Console.WriteLine("*** Cadeias de Markov***");
             Console.WriteLine("Entre com a matriz P (tamanho 3x3): ");
 
-            CarregaMatriz(matrizP);
+            CarregaMatrizP(matrizP);
+
+            Console.WriteLine("Digite M : ");
+            CarregaMatrizM(matrizM);
 
             Console.WriteLine("Digite n: ");
             int n = int.Parse(Console.ReadLine());
 
             var pDeN = CalculaPdeN(matrizP, matrizP, n);
 
+            var m = CalculaM(matrizM, pDeN);
             ImprimeMatriz(pDeN, n);
+            ImprimeMM(m, n);
 
             Console.ReadLine();
         }
 
-        private static void ImprimeMatriz(double[,] pDeN, int n)
+        private static double[,] CalculaM(double[,] matrizM, double[,] pDeN)
         {
-            Console.WriteLine($"****P({n}) : ****");
-            Console.Write($"[{Math.Round(pDeN[0, 0], 3)} ");
-            Console.Write($"{Math.Round(pDeN[0, 1], 3)} ");
-            Console.WriteLine($"{Math.Round(pDeN[0, 2], 3)}]");
-            Console.Write($"[{Math.Round(pDeN[1, 0], 3)} ");
-            Console.Write($"{Math.Round(pDeN[1, 1], 3)} ");
-            Console.WriteLine($"{Math.Round(pDeN[1, 2], 3)}]");
-            Console.Write($"[{Math.Round(pDeN[2, 0], 3)} ");
-            Console.Write($"{Math.Round(pDeN[2, 1], 3)} ");
-            Console.WriteLine($"{Math.Round(pDeN[2, 2], 3)}]");
+            var resultado = new double[1,3];
+            for (int i = 0; i < 3; i++)
+            {
+                resultado[0,i] = ProdutoEscalar(0, i, matrizM, pDeN);
+            }
+
+            return resultado;
         }
 
-        private static double[,] CalculaPdeN(double[,] matrizP, double[,] matrizB, int n)
+        private static double[,] CalculaPdeN(double[,] matrizA, double[,] matrizB, int n)
         {
             if (n == 1)
-                return matrizP;
+                return matrizA;
 
             var resultado = new double[3, 3];
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
                 {
-                    resultado[i, j] = ProdutoEscalar(i, j, matrizP, matrizB);
+                    resultado[i, j] = ProdutoEscalar(i, j, matrizA, matrizB);
                 }
 
-            return CalculaPdeN(resultado, matrizP, n-1);
+            return CalculaPdeN(resultado, matrizA, n-1);
         }
 
         private static double ProdutoEscalar(int i, int j, double[,] matrizA, double[,] matrizB)
@@ -64,7 +67,7 @@ namespace CadeiasDeMarkov
             return total;
         }
 
-        private static void CarregaMatriz(double[,] matrizP)
+        private static void CarregaMatrizP(double[,] matrizP)
         {
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
@@ -73,6 +76,38 @@ namespace CadeiasDeMarkov
                     var v = Console.ReadLine();
                     matrizP[i, j] = double.Parse(v);
                 }
+        }
+
+        private static void CarregaMatrizM(double[,] matrizM)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Digite o valor {i} de M : ");
+                var v = Console.ReadLine();
+                matrizM[0, i] = double.Parse(v);
+            }
+        }
+
+        private static void ImprimeMatriz(double[,] pDeN, int n)
+        {
+            Console.WriteLine($"\n**** P({n}): ****");
+            Console.Write($"[{Math.Round(pDeN[0, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[0, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[0, 2], 3)}]");
+            Console.Write($"[{Math.Round(pDeN[1, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[1, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[1, 2], 3)}]");
+            Console.Write($"[{Math.Round(pDeN[2, 0], 3)} ");
+            Console.Write($"{Math.Round(pDeN[2, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(pDeN[2, 2], 3)}]");
+        }
+
+        private static void ImprimeMM(double[,] m, int n)
+        {
+            Console.WriteLine($"\n**** M({n}): ****");
+            Console.Write($"[{Math.Round(m[0, 0], 3)} ");
+            Console.Write($"{Math.Round(m[0, 1], 3)} ");
+            Console.WriteLine($"{Math.Round(m[0, 2], 3)}]");
         }
     }
 }
